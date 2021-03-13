b1c437afbbe0887e2b703506790848b7b83fb827
ArjunM123 <76185915+ArjunM123@users.noreply.github.com>
2021-03-13 13:57:26 -0500

    fdafdsa
    fdasfdsafsa

---
+++ R5-App/Assets/Script/ReciveData.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Net;
+using System.IO;
+using UnityEngine;
+
+public class ReciveData : MonoBehaviour
+{
+    const string URL = "https://personalpedometer.tk/api/data";
+    string[] data;
+
+    void Update()
+    {
+        HttpWebRequest request = (HttpWebRequest)WebRequest.Create(URL);
+        request.Method = "GET";
+
+        var webResponse = request.GetResponse();
+        var webStream = webResponse.GetResponseStream();
+        var responseReader = new StreamReader(webStream);
+        var response = responseReader.ReadToEnd();
+        //print("Response: " + response);
+        responseReader.Close();
+
+        for (int i = 0; i < response.Length; i++) 
+        {
+            
+        }
+    }
+    
+    string[] userData()
+}
+
